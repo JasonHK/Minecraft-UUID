e3c89cd0b0644015e2e85e1f5e0a924a11bc3124
Kwok Chi Leong <JasonHK@users.noreply.github.com>
2018-09-09 17:36:26 +0800

    Fixed mixed content

--- js/mojang.js
+++ js/mojang.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function getUserInfo(username) {
-    const baseUrl = "http://cors.io/?https://api.mojang.com/users/profiles/minecraft/";
+    const baseUrl = "https://cors.io/?https://api.mojang.com/users/profiles/minecraft/";
     let requestUrl = baseUrl + username;
 
     var request = new Request(requestUrl, {
