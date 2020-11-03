143b5bc7347d347cc976cb229b4e1ad2d8a5e36f
Jason Kwok <JasonHK@users.noreply.github.com>
2020-11-03 12:20:59 +0800

    Added title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/pro-solid-svg-icons";
-import React, { FC } from "react";
+import Head from "next/head";
+import React, { FC, Fragment } from "react";
 
 import Container from "@/components/Container";
 
 const IndexPage: FC = () =>
 {
     return (
-        <Container
-            title="What's my UUID?"
-            subtitle="Check the UUID of your Minecraft username with a click!"
-            icon={ <FontAwesomeIcon icon={ faUser } /> }
-        ></Container>
+        <Fragment>
+            <Head>
+                <title>What's my Minecraft UUID?</title>
+            </Head>
+            <Container
+                title="What's my UUID?"
+                subtitle="Check the UUID of your Minecraft username with a click!"
+                icon={ <FontAwesomeIcon icon={ faUser } /> }
+            ></Container>       
+        </Fragment>
     );
 };
 
